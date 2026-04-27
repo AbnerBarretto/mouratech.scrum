0abfbff9d45e30b334e4bc714f0e1e2f3ba91abd
Abner Lucas <abnerlc4@gmail.com>
2026-04-27 09:17:12 -0300

    Sync all local changes: data and generated files

--- purchases-data.js
+++ purchases-data.js
@@ -1,37 +1,13 @@
 window.purchasesData = [
   {
     "userId": "user-1777151900725",
-    "eventId": "capital-inicial",
-    "amount": 220.5,
-    "cardName": "Abner Barreto",
-    "id": "buy-1777157601978",
-    "date": "2026-04-25T22:53:21.979Z"
-  },
-  {
-    "userId": "user-1777151900725",
-    "eventId": "capital-inicial",
-    "amount": 210,
-    "cardName": "ABNER BARRETO",
-    "id": "buy-1777158701047",
-    "date": "2026-04-25T23:11:41.047Z"
-  },
-  {
-    "userId": "user-1777151900725",
-    "eventId": "djavan-turne-d",
-    "amount": 160,
-    "cardName": "ABNER BARRETO",
-    "id": "buy-1777159070762",
-    "date": "2026-04-25T23:17:50.762Z"
-  },
-  {
-    "userId": "user-1777151900725",
     "eventId": "guns-n-roses",
     "amount": 365.37,
     "quantity": 1,
     "unitPrice": 325.2,
     "serviceFee": 10,
     "interestAmount": 30.168,
-    "cardName": "ABNER BARRETO",
+    "cardName": "Usuário",
     "installments": "7",
     "id": "buy-1777225000744",
     "date": "2026-04-26T17:36:40.744Z"
